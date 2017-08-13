35518edbd24a640407299c6b4021a128cb4215e3
Simarpreet Singh <simar@linux.com>
2017-08-13 10:00:46 +0530

    constants: Run all servers on localhost
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- constants.go
+++ constants.go
@@ -1,18 +1,18 @@
 package main
 
 // DHCP Server
-const DHCP_SERVER_ADDR = "172.10.0.1"
+const DHCP_SERVER_ADDR = "127.0.0.1"
 const DHCP_SERVER_PORT = 67
-const DHCP_SERVER_LEASE_START_ADDR = "172.10.0.2"
+const DHCP_SERVER_LEASE_START_ADDR = "127.0.0.2"
 const DHCP_LEASE_DURATION = 10
 const DHCP_LEASE_COUNT = 10
 const DHCP_LEASE_RANGE = 50
 
 // TFTP Server
-const TFTP_SERVER_ADDR = "172.10.0.1"
+const TFTP_SERVER_ADDR = "127.0.0.1"
 const TFTP_SERVER_PORT = 69
 const PXELINUX_LOADER = "undionly.kpxe"
 
 // DNS Server
-const DNS_SERVER_ADDR = "172.10.0.1"
+const DNS_SERVER_ADDR = "127.0.0.1"
 const DNS_SERVER_PORT = 53
