8188d26b213090230479d061629479da895cba41
Simarpreet Singh <simar@linux.com>
2017-11-06 22:42:04 -0800

    dns: Support DNS with NS1
    This adds the ability to use NS1 as the
    DNS backend for xserver. Now supports any domain
    and any zone.
    
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- dns.go
+++ dns.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 
 	"github.com/miekg/dns"
+	"math/rand"
 	log "github.com/sirupsen/logrus"
+	ns1 "gopkg.in/ns1/ns1-go.v2/rest"
+	"net/http"
+	"time"
+	"os"
+	"strings"
 )
+
+var ns1Client *ns1.Client
 
 type dnsServerHandler struct {
 	ds *dns.Server
 }
 
-// TODO: Move in-memory mapping toa real datastore
-var records = map[string]string{
-	"foo.com.": "192.168.0.1",
+func getRandomAnswer(max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max - 0) + 0
 }
 
 func parseQuery(m *dns.Msg) {
@@ -22,16 +30,29 @@ func parseQuery(m *dns.Msg) {
 	for _, q := range m.Question {
 		switch q.Qtype {
 		case dns.TypeA:
-			rl.Info("Query for: ", q.Name)
-			ip := records[q.Name]
-			if ip != "" {
-				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+			zone := q.Name[strings.IndexAny(q.Name, ".")+1:len(q.Name)-1]
+			domain := q.Name[:strings.LastIndex(q.Name, ".")]
+
+			rl.Info("Query for > ", "domain: ", domain, " zone: ", zone, " type: ", q.Qtype)
+			record, _, err := ns1Client.Records.Get(zone, domain, "A")
+			if err != nil {
+				rl.Error("Record: ", q.Name, err)
+			} else {
+				rl.Info(record.Answers)
+				var answers []string
+				for _, answer := range record.Answers {
+					answers = append(answers, answer.String())
+				}
+				// TODO: Implement RR answer retrieval
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, answers[getRandomAnswer(len(answers))]))
 				if err == nil {
 					m.Answer = append(m.Answer, rr)
+				} else {
+					rl.Error(err)
 				}
-			} else {
-				rl.Error("Record: ", q.Name, " not found")
 			}
+
+		// TODO: Add IPv6 support
 		default:
 			rl.Info("Query type: ", q.Qtype, " is currently not supported")
 		}
@@ -63,12 +84,19 @@ func newDefaultDNSServerHandler() *dnsServerHandler {
 		ds: newDNSServerHandler(DNS_SERVER_ADDR, DNS_SERVER_PORT, "udp"),
 	}
 }
+func init() {
+	k := os.Getenv("NS1_APIKEY")
+	if k == "" {
+		fmt.Println("NS1_APIKEY environment variable is not set, stopping DNS server")
+	}
+	httpClient := &http.Client{Timeout: time.Second * 10}
+	ns1Client = ns1.NewClient(httpClient, ns1.SetAPIKey(k))
+}
 
 func (h *dnsServerHandler) RouteDNS() error {
 	rl := NewLogger(log.New().Writer())
 
-	// TODO: Add more domains to the serving list
-	dns.HandleFunc("com.", serve)
+	dns.HandleFunc(".", serve)
 	err := h.ds.ListenAndServe()
 	if err != nil {
 		rl.Error("Failed to start DNS server", err)
